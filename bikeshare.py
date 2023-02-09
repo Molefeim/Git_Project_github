26a49290f48bdb8963930fb20c764c567f32cbcf
[Ishmael Molefe] <[imolefe2@debswana.bw]>
2023-02-09 11:29:01 +0200

    refactor: Update code comment

--- bikeshare.py
+++ bikeshare.py
@@ -14,7 +14,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     print("Would you like to see data for chicago, new york city or washington:?")
     cityname = input().lower() 
     # get user input for month (all, january, february, ... , june)
